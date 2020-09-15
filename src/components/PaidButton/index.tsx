312429b43da17123917b8efc7cbf4aa9e8e01cc8
rafanel2 <38199191+rafanel2@users.noreply.github.com>
2020-09-14 23:29:41 -0300

    Created Paid Button component

---
+++ src/components/PaidButton/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import styles from './styles';
+
+interface PaidButtonProps {
+    paid: boolean,
+    onChange: Function,
+}
+
+
+const PaidButton: React.FC<PaidButtonProps> = ({ paid, onChange }) => {
+
+    return (
+        <View style={styles.inputContainer} >
+        <Text style={styles.inputTitle}>Paid</Text>
+        <View style={styles.optionContainer}>
+            <TouchableOpacity
+                style={[styles.optionButton, paid && styles.active]}
+                onPress={() => onChange(true)}
+            >
+                <Text style={[styles.optionTitle, paid && styles.textActive]} >Paid</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={[styles.optionButton, !paid && styles.active]}
+                onPress={() => onChange(false)}
+
+            >
+                <Text style={[styles.optionTitle, !paid && styles.textActive]} >Not Paid</Text>
+            </TouchableOpacity>
+
+        </View>
+    </View>
+    );
+
+}
+
+export default PaidButton;
---
+++ src/components/PaidButton/styles.ts
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native'; 
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 10,
+        marginBottom: 8
+    },
+    inputTitle: {
+        color: '#b9b9b9',
+        fontSize: 12,
+        fontFamily: 'Roboto_500Medium'
+    },
+    optionContainer: {
+        marginTop: 2,
+        height: 40,
+        borderRadius: 10,
+        borderWidth: 0.5,
+        borderColor: '#dedede',
+        flexDirection: 'row',
+        justifyContent: "space-between"
+    },
+    optionButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+    },
+    optionTitle: {
+        fontFamily: 'Roboto_700Bold',
+        color: '#b9b9b9',
+        fontSize: 13
+    },
+    active: {
+        borderRadius: 10,
+        color: '#fff',
+        backgroundColor: '#4643d3'
+    },
+    textActive: {
+        color: '#fff',
+    },
+});
+
+export default styles;
