197289affabca16f18cc9de9e3e17583a0945351
rafanel2 <38199191+rafanel2@users.noreply.github.com>
2020-09-14 23:28:21 -0300

    Add page header component

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+interface PageHedaerProps {
+    title: string
+}
+
+const PageHeader: React.FC<PageHedaerProps> = ({ title }) => {
+
+    const { goBack } = useNavigation();
+
+    function handleBack() {
+        goBack();
+    }
+
+
+    return (
+        <View style={styles.container} >
+            <View style={styles.topBar}>
+                <TouchableOpacity onPress={
+                    () => handleBack()
+                }>
+                    <AntDesign name="arrowleft" size={24} color="#fff" />
+                </TouchableOpacity>
+                <Text style={styles.title} >{title}</Text>
+
+                <View></View>
+            </View>
+
+        </View >
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 40,
+        paddingTop: 60,
+        backgroundColor: '#4643d3',
+        height: 160
+    },
+    topBar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    title: {
+        color: '#fff',
+        fontFamily: 'Roboto_700Bold',
+        fontSize: 18
+    }
+});
+
+export default PageHeader;
