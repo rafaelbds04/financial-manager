6c4519d86ff8daffb86ecefb52222a6453fb35e6
rafanel2 <38199191+rafanel2@users.noreply.github.com>
2020-10-16 15:41:42 -0300

    Added onPress on transaction card.

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -1,51 +1,55 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, GestureResponderEvent } from 'react-native';
 import { CategoryType } from '../../pages/AddTransaction';
 import styles from './styles';
 import { AntDesign } from '@expo/vector-icons';
 import { Transactions } from '../../pages/Home';
 import moment from 'moment';
 import accounting from 'accounting';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 interface TransactionCardProps {
-    data: Transactions
+    data: Transactions,
+    onPress?: ((event: GestureResponderEvent) => void) 
 }
 
-const TransactionCard: React.FC<TransactionCardProps> = ({ data }) => {
+const TransactionCard: React.FC<TransactionCardProps> = ({ data, onPress }) => {
     return (
-        <View style={styles.transactionItemCard}>
-            <View style={{ flexDirection: 'row' }}>
-                <View style={styles.transactionItemIcon}>
-                    {data.transactionType == CategoryType.REVENUE ?
-                        <AntDesign name="arrowup"
-                            size={24} color="#43b864" />
-                        :
-                        <AntDesign name="arrowdown"
-                            size={24} color="#e95e51" />
-                    }
+        <TouchableOpacity onPress={onPress}>
+            <View style={styles.transactionItemCard}>
+                <View style={{ flexDirection: 'row' }}>
+                    <View style={styles.transactionItemIcon}>
+                        {data.transactionType == CategoryType.REVENUE ?
+                            <AntDesign name="arrowup"
+                                size={24} color="#43b864" />
+                            :
+                            <AntDesign name="arrowdown"
+                                size={24} color="#e95e51" />
+                        }
+                    </View>
+                    <View style={{ left: 30 }} >
+                        <Text style={styles.transactionItemTitle}>
+                            {data.name.length < 15 ? data.name : `${data.name.substring(0, 15)}...`}</Text>
+                        <Text style={styles.transactionItemSubtitle} >
+                            {moment(data.transactionDate).format('DD MMM, HH:mm')}</Text>
+                    </View>
                 </View>
-                <View style={{ left: 30 }} >
-                    <Text style={styles.transactionItemTitle}>
-                        {data.name.length < 15 ? data.name : `${data.name.substring(0, 15)}...`}</Text>
-                    <Text style={styles.transactionItemSubtitle} >
-                        {moment(data.transactionDate).format('DD MMM, HH:mm')}</Text>
+                <View style={{ justifyContent: 'center' }}>
+                    <Text style={{
+                        ...styles.transactionItemValue,
+                        color: `${data.transactionType == CategoryType.REVENUE ? '#43b864' : '#fff'}`
+                    }} >
+                        {data.transactionType == CategoryType.REVENUE ? '' : '-'}
+                        {accounting.formatMoney(Number(data.amount), {
+                            decimal: ',',
+                            thousand: '.',
+                            precision: 2,
+                            symbol: 'R$'
+                        }).toString()}
+                    </Text>
                 </View>
             </View>
-            <View style={{ justifyContent: 'center' }}>
-                <Text style={{
-                    ...styles.transactionItemValue,
-                    color: `${data.transactionType == CategoryType.REVENUE ? '#43b864' : '#fff'}`
-                }} >
-                    {data.transactionType == CategoryType.REVENUE ? '' : '-'}
-                    {accounting.formatMoney(Number(data.amount), {
-                        decimal: ',',
-                        thousand: '.',
-                        precision: 2,
-                        symbol: 'R$'
-                    }).toString()}
-                </Text>
-            </View>
-        </View>
+        </TouchableOpacity>
     );
 }
 
